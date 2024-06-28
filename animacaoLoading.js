e41b5df862ef6d6f8176aa9dbbedd37f9bce1147
Vkzz <joojaovitor2742@gmail.com>
2024-06-28 16:17:49 -0300

    Central links com tela de carregamento

--- animacaoLoading.js
+++ animacaoLoading.js
@@ -1,16 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let width = 0;
-    let interval = setInterval(function() {
-        if (width >= 100) {
-            clearInterval(interval);
-            document.getElementById('loading-screen').style.opacity = 0;
-            setTimeout(function() {
-                document.getElementById('loading-screen').style.display = 'none';
-            }, 500);
-        } else {
-            width++;
-            document.getElementById('loading-bar').style.width = width + '%';
-            document.getElementById('loading-text').innerText = width + '%';
-        }
-    }, 10); 
-});
+gsap.fromTo('.loading-page' , {opacity:1},{
+    opacity:0,
+    duration:1,
+    delay:1,
+})
